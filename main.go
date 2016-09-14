28f3a904818f9ec6cf523dc6de392b0cc70f92bd
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2016-09-14 12:37:05 +0300

    Add main.

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/AlekSi/vscode-delve-example/lib"
+)
+
+func main() {
+	v1, err := lib.NewVersion("1.1.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	v2, err := lib.NewVersion("1.2.0")
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Print(v1.Less(v2))
+}
